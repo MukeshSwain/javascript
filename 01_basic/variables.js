49c2420d5ceb575754fc0208f52065ecc9754372
MukeshSwain <161455068+MukeshSwain@users.noreply.github.com>
2024-07-28 10:36:59 +0000

    variables

---
+++ 01_basic/variables.js
@@ -0,0 +1,13 @@
+const m = 10;
+console.log(m);
+let name = "Mukesh"
+var city = "Rayagada"
+variable = 123455
+/*
+prefer not to use var 
+because of blck problem and functional problem
+*/
+let nis ;
+// m = 29 re initialization to a variable of type const is not allowed
+console.table([m,name,city,variable,nis])
+// And if a variable is declared but not initialized then it holds undefined value
