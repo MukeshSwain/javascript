858a6939b2645137e7c9e92af1410b93f1d953d5
MukeshSwain <161455068+MukeshSwain@users.noreply.github.com>
2024-08-01 03:26:57 +0000

    iifee....

---
+++ 01_basic/iife.js
@@ -0,0 +1,14 @@
+/*
+    Immediately Invoked Function Expression (IIFE)
+    To avoid global polution problems we use iife and to end its execution we
+    must put a semicolon at the end 
+    ()();
+*/
+// Syntax for iife or example ..
+(function chai(){//Named IIFe
+    console.log("Database Connected :")
+})();
+
+((argu)=>{//Unamed IIFE..
+    console.log(`Db two also connected by ${argu}`)
+})("mukesh");
